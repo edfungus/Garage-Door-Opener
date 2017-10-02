759f2675d64f49e68b885845441bb6a3f0a92423
edfungus <edfungus@gmail.com>
2017-10-01 23:18:29 -0700

    Added wolf.jpg route

--- main.go
+++ main.go
@@ -10,10 +10,14 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
+func wolf(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "wolf.jpg")
+}
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
+	r.HandleFunc("/wolf.jpg", wolf)
 
 	log.Fatal(http.ListenAndServe(":80", r))
 }
